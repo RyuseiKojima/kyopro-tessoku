54de975d99e3e6f402ade12c2b6895873367da8f
RyuseiKojima <jiko.likegc@gmail.com>
2025-07-27 22:00:33 +0900

    Add Answer_B31: Implement calculation logic for multiples of 3, 5, and additional factors

--- codes/java/chap05/answer_B31.java
+++ codes/java/chap05/answer_B31.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Answer_A31 {
+class Answer_B31 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
@@ -9,7 +9,11 @@ class Answer_A31 {
 		sc.close();
 		long A1 = N /  3; // 3 で割り切れるものの個数
 		long A2 = N /  5; // 5 で割り切れるものの個数
-		long A3 = N / 15; // 3, 5 両方で割り切れるもの（= 15 の倍数）の個数
-		System.out.println(A1 + A2 - A3);
+		long A3 = N / 7;
+		long A4 = N / 15;
+		long A5 = N / 21;
+		long A6 = N / 35;
+		long A7 = N / 105;
+		System.out.println(A1 + A2 + A3 - A4 - A5 - A6 + A7);
 	}
 };
