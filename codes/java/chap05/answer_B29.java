a23daeeca9e83045c2ad1813783813b64c8f8c8c
RyuseiKojima <jiko.likegc@gmail.com>
2025-07-27 19:07:42 +0900

    B29

--- codes/java/chap05/answer_B29.java
+++ codes/java/chap05/answer_B29.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Answer_A29 {
+class Answer_B29 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
@@ -17,9 +17,9 @@ class Answer_A29 {
 	// a の b 乗を m で割った余りを返す関数
 	// 変数 a は a^1 → a^2 → a^4 → a^8 → a^16 → ･･･ と変化
 	static long Power(long a, long b, long m) {
-		long p = a, Answer = 1;
-		for (int i = 0; i < 30; i++) {
-			int wari = (1 << i);
+		long p = a % m, Answer = 1;
+		for (int i = 0; i < 60; i++) {
+			long wari = (1L << i);
 			if ((b / wari) % 2 == 1) {
 				Answer = (Answer * p) % m; // 「a の 2i 乗」が掛けられるとき
 			}
