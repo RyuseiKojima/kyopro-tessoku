7b44ac5b6ac5c8c281e78c1bdec3cacc5ce9b549
RyuseiKojima <jiko.likegc@gmail.com>
2025-07-27 22:14:55 +0900

    Refactor Answer_A32: Rename class from Main to Answer_A32 and close scanner after input Add Answer_B32: Implement winner calculation logic with dynamic programming for multiple inputs

--- codes/java/chap05/answer_B32.java
+++ codes/java/chap05/answer_B32.java
@@ -1,22 +1,26 @@
 import java.util.*;
 
-class Answer_A32 {
+class Answer_B32 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		// 入力
 		int N = sc.nextInt();
-		int A = sc.nextInt();
-		int B = sc.nextInt();
+		int K = sc.nextInt();
+		int[] A = new int[K];
+		for (int i = 0; i < K; i++) A[i] = sc.nextInt();
 
 		sc.close();
 
 		// 勝者を計算する
 		boolean[] dp = new boolean[N + 1];
 		for (int i = 0; i <= N; i++) {
-			if (i >= A && dp[i-A] == false) dp[i] = true; // 勝ちの状態
-			else if (i >= B && dp[i-B] == false) dp[i] = true; // 勝ちの状態
-			else dp[i] = false; // 負けの状態
+			for (int j = 0; j < K; j++) {
+				if (i >= A[j] && !dp[i - A[j]]) {
+					dp[i] = true; // 勝ちの状態
+					break;
+				}
+			}
 		}
 
 		// 出力
