a3fa67fb4ac1f9257715069042e36ecbc0b0ce3b
RyuseiKojima <jiko.likegc@gmail.com>
2025-07-27 16:01:14 +0900

    B27

--- codes/java/chap05/answer_B27.java
+++ codes/java/chap05/answer_B27.java
@@ -1,22 +1,22 @@
 import java.util.*;
 
-class Answer_A27 {
+class Answer_B27 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		// 入力
-		int A = sc.nextInt();
-		int B = sc.nextInt();
+		long A = sc.nextLong();
+		long B = sc.nextLong();
 
 		sc.close();
 
 		// 出力
-		System.out.println(GCD(A, B));
+		System.out.println((A * B) / GCD(A, B));
 	}
 
 	// 正の整数 A と B の最大公約数を返す関数
 	// GCD は Greatest Common Divisor（最大公約数）の略
-	static int GCD(int A, int B) {
+	static long GCD(long A, long B) {
 		while (A >= 1 && B >= 1) {
 			if (A >= B) {
 				A %= B; // A の値を変更する場合
