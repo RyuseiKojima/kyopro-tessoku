45e7d46ec397ac3ee8da2a5da760d569acb2305d
RyuseiKojima <jiko.likegc@gmail.com>
2025-08-09 12:22:09 +0900

    Add Answer_C01 class to calculate 10% increase of input value

---
+++ codes/java/chap10/answer_C01.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class Answer_C01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		sc.close();
+		System.out.println((int)(N * 1.1));
+	}
+}
