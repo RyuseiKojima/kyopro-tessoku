dfe4288499feb077625660c9e6fa5d8f17c5c5aa
RyuseiKojima <jiko.likegc@gmail.com>
2025-08-11 07:40:19 +0900

    Rename Main class to Answer_B61 and implement adjacency list creation with output functionality

--- codes/java/chap09/answer_B61.java
+++ codes/java/chap09/answer_B61.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-class Answer_A61 {
+class Answer_B61 {
 	public static void main(String[] args) throws IOException {
 		// 入力（高速な入力のため、Scanner の代わりに BufferedReader を使っています）
 		BufferedReader buff = new BufferedReader(new InputStreamReader(System.in));
@@ -30,16 +30,15 @@ class Answer_A61 {
 		// 出力（G[i].size() は頂点 i に隣接する頂点のリストの大きさ ＝ 次数）
 		//（高速な出力のため、System.out.println ではなく PrintWriter を使っています）
 		PrintWriter output = new PrintWriter(System.out);
+		int ans = 0;
+		int max = 0;
 		for (int i = 1; i <= N; i++) {
-			output.print(i + ": {");
-			for (int j = 0; j < G[i].size(); j++) {
-				if (j >= 1) {
-					output.print(", ");
-				}
-				output.print(G[i].get(j)); // G[i].get(j) は頂点 i に隣接する頂点のうち j+1 番目のもの
+			if (G[i].size() > max) {
+				max = G[i].size();
+				ans = i;
 			}
-			output.println("}");
 		}
+		output.println(ans);
 		output.flush();
 	}
 }
