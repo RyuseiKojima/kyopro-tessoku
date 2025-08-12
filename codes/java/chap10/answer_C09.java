503cef64166dc3092c401937ea8f60527186faf2
RyuseiKojima <jiko.likegc@gmail.com>
2025-08-12 16:23:59 +0900

    Add Answer_C09 class for input processing and dynamic programming solution

---
+++ codes/java/chap10/answer_C09.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Answer_C09 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] A = new int[N + 1];
+		for (int i = 1; i <= N; i++) {
+			A[i] = sc.nextInt();
+		}
+		sc.close();
+
+		long[] dp = new long[N + 1];
+		dp[1] = A[1];
+		dp[2] = Math.max(A[1], A[2]);
+
+		for (int i = 3; i <= N; i++) {
+			dp[i] = Math.max(dp[i - 1], dp[i - 2] + A[i]);
+		}
+
+		System.out.println(dp[N]);
+	}
+}
